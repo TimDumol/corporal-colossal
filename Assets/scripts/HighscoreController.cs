00d84ed8645bd362057f808140cc1186d70d5b02
Tim Dumol <tim@timdumol.com>
2015-01-24 19:51:35 +0800

    Fix enemy tag.

--- Assets/scripts/HighscoreController.cs
+++ Assets/scripts/HighscoreController.cs
@@ -5,9 +5,9 @@ public class HighscoreController : MonoBehaviour {
 	public static SortedDictionary<string, int> highscoreDict;
 
 	void Awake () {
-		string serializedHighscores = PlayerPrefs.GetString ();
-		foreach (string strTuple in serializedHighscores.Split (";")) {
-			string[] splitted = strTuple.Split (",");
+		string serializedHighscores = PlayerPrefs.GetString ("highscores");
+		foreach (string strTuple in serializedHighscores.Split (";".ToCharArray())) {
+			string[] splitted = strTuple.Split (",".ToCharArray());
 			highscoreDict[splitted[0]] = int.Parse(splitted[1]);
 		}
 	}
