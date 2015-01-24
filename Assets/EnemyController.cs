9232f1598b721076c1c34716582f598733b0ed3a
Payton Yao <payton.yao@gmail.com>
2015-01-24 13:22:45 +0800

    Added script that moves the enemy

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	private NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+		navAgent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] sheeps = GameObject.FindGameObjectsWithTag ("Sheep");
+		float distance = float.MaxValue;
+		GameObject closestSheep = null;
+
+		foreach(GameObject sheep in sheeps) {
+			Vector3 v = this.transform.position - sheep.transform.position;
+			if (v.sqrMagnitude < distance) {
+				closestSheep = sheep;
+				distance = v.sqrMagnitude;
+			}
+		}
+
+		navAgent.SetDestination (closestSheep.transform.position);
+		Debug.Log (closestSheep);
+	}
+}
