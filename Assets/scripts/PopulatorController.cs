b016a9879bbd8b74c83f54b5459591bb84239e1b
aysonje <aysonje@gmail.com>
2015-01-24 21:07:55 +0800

    fixed populator to not spawn inside pen

--- Assets/scripts/PopulatorController.cs
+++ Assets/scripts/PopulatorController.cs
@@ -21,19 +21,27 @@ public class PopulatorController : MonoBehaviour
 		//Debug.Log (string.Format ("my size is : {0}; {1}", sheepSize, sheep.collider.bounds));
 	}
 
+	bool canPut(Vector3 size, float x, float z)
+	{
+		Vector3 penSize = GameObject.Find("Sheep Pen").transform.localScale;
+		if (Mathf.Abs(x) < penSize.x && Mathf.Abs(z) < penSize.z)
+			return false;
+		Vector3 start = new Vector3(x, size.x / 2f + 0.5f, z - size.z / 2f);
+		Vector3 end = new Vector3(x, size.x / 2f + 0.5f, z + size.z / 2f);
+		float radius = size.x / 2f;
+		return (!Physics.CheckCapsule(start, end ,radius));
+	}
+	
 	void SpawnSheep (int level)
 	{
 		bool spawned = false;
 		do {
 			float z = Random.Range (GameProperties.bottom, GameProperties.top);
 			float x = Random.Range (GameProperties.left, GameProperties.right);
-			Debug.Log (string.Format ("Generated: {0}, {1}; sheep size is: {2} ", x, z, sheepSize));
-			if (!Physics.CheckCapsule (new Vector3(x - sheepSize.x/2f, z/2f + 0.5f, z), new Vector3(x + sheepSize.x/2f, z/2f + 0.5f, z), z/2f)) {
-				Debug.Log (string.Format ("Pass: {0}, {1}; sheep size is: {2} ", x, z, sheepSize));
+			if (canPut(sheepSize, x, z)) {
 				Instantiate (sheep, new Vector3 (x, 0.5f, z), Quaternion.identity);
 				spawned = true;
 			}
-
 		} while (!spawned);
 	}
 
@@ -43,14 +51,11 @@ public class PopulatorController : MonoBehaviour
 		do {
 			float z = Random.Range (GameProperties.bottom, GameProperties.top);
 			float x = Random.Range (GameProperties.left, GameProperties.right);
-			Debug.Log (string.Format ("Generated: {0}, {1}; enemy size is: {2} ", x, z, enemySize));
-			if (!Physics.CheckCapsule (new Vector3(x - enemySize.x/2f, z/2f + 0.5f, z), new Vector3(x + enemySize.x/2f, z/2f + 0.5f, z), z/2f)) {
-				Debug.Log (string.Format ("Pass: {0}, {1}; enemy size is: {2} ", x, z, enemySize));
+			if (canPut(enemySize, x, z)) {
 				GameObject e = (GameObject)Instantiate (enemy, new Vector3 (x, 0.5f, z), Quaternion.identity);
 				e.GetComponent<NavMeshAgent>().speed *= 1 + 0.20f*level;
-                spawned = true;
-            }
-            
+				spawned = true;
+			}
         } while (!spawned);
 	}
 
