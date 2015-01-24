5d114d6a61c0bb17243f046aa6c86a6edd477d13
Tim Dumol <tim@timdumol.com>
2015-01-24 15:47:38 +0800

    Try to populate better.

--- Assets/scripts/PopulatorController.cs
+++ Assets/scripts/PopulatorController.cs
@@ -13,7 +13,8 @@ public class PopulatorController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		sheepSize = (sheep.collider as BoxCollider).size;
+		sheepSize = sheep.transform.localScale;
+		//Debug.Log (string.Format ("my size is : {0}; {1}", sheepSize, sheep.collider.bounds));
 	}
 
 	void SpawnSheep ()
@@ -24,6 +25,7 @@ public class PopulatorController : MonoBehaviour
 			float x = Random.Range (GameProperties.left, GameProperties.right);
 			Debug.Log (string.Format ("Generated: {0}, {1}; sheep size is: {2} ", x, z, sheepSize));
 			if (Physics.CheckCapsule (new Vector3(x - sheepSize.x/2f, z/2f + 0.5f, z), new Vector3(x + sheepSize.x/2f, z/2f + 0.5f, z), z/2f)) {
+				Debug.Log (string.Format ("Pass: {0}, {1}; sheep size is: {2} ", x, z, sheepSize));
 				Instantiate (sheep, new Vector3 (x, 1, z), Quaternion.identity);
 				spawned = true;
 			}
