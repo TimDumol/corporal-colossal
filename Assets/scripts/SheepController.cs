295969f1725976a935163cbbe2c48ba5fec68f3b
aysonje <aysonje@gmail.com>
2015-01-24 15:33:17 +0800

    Added SheepController, fixed Enemy behavior

---
+++ Assets/scripts/SheepController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepController : MonoBehaviour
+{
+	private NavMeshAgent navAgent;
+
+	public float moveSpeed = 5;
+
+	void Start ()
+	{
+		navAgent = this.GetComponent<NavMeshAgent>();
+	}
+
+	void Update ()
+	{
+		float dx = Random.Range (-10.0f, 10.0f) * moveSpeed;
+		float dz = Random.Range (-10.0f, 10.0f) * moveSpeed;
+
+		Vector3 targetPosition = transform.position + new Vector3(dx, 0, dz);
+		navAgent.SetDestination(targetPosition);
+	}
+}
